11f60021bf38d1ba06711c7227d27f7bacea6099
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-28 10:28:11 +0200

    Move item to seperate component

---
+++ app/components/genres/list-item/item/component.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+const { Component, computed } = Ember;
+
+export default Component.extend({
+  tagName: 'tr',
+
+  movie: computed('genre', function() {
+    return this.get('genre.movies');
+  }),
+});
