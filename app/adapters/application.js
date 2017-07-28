95d49297675b4b1c12d5ba0f69f8a3900472f0c9
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-28 10:29:28 +0200

    Code structure refactor

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
+const { JSONAPIAdapter } = DS;
+
+export default JSONAPIAdapter.extend({
   namespace: 'api',
   host: 'http://localhost:3000'
 });
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPISerializer.extend({
+const { JSONAPISerializer } = DS;
+
+export default JSONAPISerializer.extend({
 });
